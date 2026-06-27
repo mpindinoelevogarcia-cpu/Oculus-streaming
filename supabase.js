5c0512aadeb2447dbabcd0e2187f6814e3cfcad7
mpindinoelevogarcia-cpu <mpindinoelevogarcia@gmail.com>
2026-06-27 15:24:24 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // Configuração de conexão do Oculus Streaming com o Supabase
-const SUPABASE_URL = "Cole_Aqui_O_Seu_Project_URL";
-const SUPABASE_KEY = "sb_publishable_YLYSpSWDa6-h0Fsihf0tuw_iKZdCM..."; // Clique no botão copiar ao lado da Publishable key e cole aqui tudo
+const SUPABASE_URL = "https://supabase.co";
+const SUPABASE_KEY = "sb_publishable_YLYSpSWDa6-h0Fsihf0tuw_iKZdCMwXmKxP0b_K88_3Rsmb1K5-PZ_g"; 
 
 // Inicializa o Supabase no site
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
